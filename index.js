02d159b30d9a22ea3d1534ad8afb46ebf64b342a
ajedkrap <mr.thedja@gmail.com>
2020-10-07 00:34:31 +0700

    submit

---
+++ index.js
@@ -0,0 +1,10 @@
+function tugas18(x){
+  split = x.split(" ");
+  console.log(split[3]+" "+split[4]);
+  slice = x.slice(8,24);
+  console.log(slice);
+}
+
+var kalimat = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout";
+
+tugas18(kalimat);
